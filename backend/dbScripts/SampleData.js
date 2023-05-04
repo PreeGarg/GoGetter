7b71dd4eb88d28eb3987d92b923ef7968cb7cb80
Preedhi Garg <preedhigarg@Preedhis-MacBook-Air.local>
2023-05-04 11:36:11 -0700

    added dates

--- backend/dbScripts/SampleData.js
+++ backend/dbScripts/SampleData.js
@@ -10,8 +10,8 @@ goalsCollection.insert(
 	title: "Intermittent Fasting",
 	description: "Follow 16:8 fasting rule for 30 days",
 	userId: "1",
-	startDate: new Date(),
-	endDate: new Date(),
+	startDate: new Date("2023-03-25"),
+	endDate: new Date("2023-04-25"),
 	category: "Health",
 	progress: "Not Started",
 	reminder: false,
@@ -23,8 +23,8 @@ goalsCollection.insert(
 	title: "Learn typescript",
 	description: "Follow typescript course on courseera",
 	userId: "1",
-	startDate: new Date(),
-	endDate: new Date(),
+	startDate: new Date("2023-04-15"),
+	endDate: new Date("2023-05-15"),
 	category: "Career",
 	progress: "Not Started",
 	reminder: false,
@@ -37,8 +37,8 @@ goalsCollection.insert(
     title: "Trekking",
     description: "Take lessons on trekking dos and donts",
     userId: "1",
-    startDate: new Date(),
-    endDate: new Date(),
+    startDate: new Date("2023-05-07"),
+    endDate: new Date("2023-06-07"),
     category: "Travel",
     progress: "Not Started",
     reminder: false,
@@ -51,8 +51,8 @@ goalsCollection.insert(
     title: "Eat more protein",
     description: "intake vegan protein",
     userId: "2",
-    startDate: new Date(),
-    endDate: new Date(),
+    startDate: new Date("2023-05-20"),
+    endDate: new Date("2023-06-20"),
     category: "Health",
     progress: "Not Started",
     reminder: false,
@@ -109,7 +109,7 @@ reminderCollection.insert({
     goalId: "1",
     userId: "1",
     message: "Do not forget to fast" ,
-    reminderDate: new Date()
+    reminderDate: new Date("2023-04-20")
 })
 
 reminderCollection.insert({
@@ -117,5 +117,5 @@ reminderCollection.insert({
     goalId: "2",
     userId: "1",
     message: "Complete your typescript lesson" ,
-    reminderDate: new Date()
+    reminderDate: new Date("2023-05-10")
 })
